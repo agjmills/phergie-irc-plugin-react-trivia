735021c91b3310b6b93abd65186f388d275e102d
Alex Mills <alex@asdfx.us>
2019-02-06 13:29:54 +0000

    refactored requirements and TypeHint

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,12 +5,11 @@ namespace Asdfx\Phergie\Plugin\Trivia;
 use Asdfx\Phergie\Plugin\Trivia\Models\User;
 use Asdfx\Phergie\Plugin\Trivia\Models\Question;
 use Phergie\Irc\Bot\React\AbstractPlugin;
-use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
 use Phergie\Irc\Event\UserEventInterface;
-use Phergie\Irc\Plugin\React\Command\CommandEvent as Event;
-use Phergie\Irc\Event\UserEvent;
+use Phergie\Irc\Bot\React\EventQueueInterface;
 
-class Plugin extends AbstractPlugin {
+class Plugin extends AbstractPlugin
+{
 
     const MODE_OFF = 0;
     const MODE_ON = 1;
@@ -28,7 +27,8 @@ class Plugin extends AbstractPlugin {
     private $database;
     private $mode = 0;
 
-    public function __construct(array $configuration = []) {
+    public function __construct(array $configuration = [])
+    {
         $this->config = $configuration;
         $this->database = new Providers\Database('sqlite', '', 'trivia.db');
     }
@@ -41,67 +41,68 @@ class Plugin extends AbstractPlugin {
         ];
     }
 
-    private function start()
+    private function start(UserEventInterface $event, EventQueueInterface $queue)
     {
         $this->mode = self::MODE_ON;
-        $this->doPrivMsg($this->config['channel'], 'I want to play some trivia, don\'t you?!');
-        $this->ask();
+        $queue->ircPrivmsg($this->config['channel'], 'I want to play some trivia, don\'t you?!');
+        $this->ask($event, $queue);
     }
 
-    public function onPrivmsg(UserEvent $event, Queue $queue)
+    public function onPrivmsg(UserEventInterface $event, EventQueueInterface $queue)
     {
         $nick = $event->getNick();
         $eventParams = $event->getParams();
-        switch($eventParams[0]) {
+        switch ($eventParams[0]) {
             case ".start":
                 if ($this->mode == self::MODE_OFF) {
-                    $this->start();
+                    $this->start($event, $queue);
                 } else {
-                    $this->doPrivMsg($this->config['channel'], 'I\'m sorry Dave, I can\'t do that right now.');
+                    $queue->ircPrivmsg($this->config['channel'], 'I\'m sorry Dave, I can\'t do that right now.');
                 }
                 break;
             case '.stop':
                 if ($this->mode != self::MODE_OFF) {
-                    $this->stop();
+                    $this->stop($event, $queue);
                 } else {
-                    $this->doPrivMsg($this->config['channel'], 'I\'m sorry Dave, I can\'t do that right now.');
+                    $queue->ircPrivmsg($this->config['channel'], 'I\'m sorry Dave, I can\'t do that right now.');
                 }
                 break;
             default:
                 if (strtolower($event->getParams()[0]) == strtolower($this->question['answer'])) {
-                    $this->correct($event);
+                    $this->correct($event, $queue);
                 } else {
                 }
                 break;
-        } 
+        }
     }
 
 
-    public function onTick()
+    public function onTick(UserEventInterface $event, EventQueueInterface $queue)
     {
         $time = time();
         if ($this->mode == self::MODE_WAITING && !is_null($this->firstHintTime) && $time >= $this->firstHintTime) {
             $this->hint = $this->getHint($this->question['answer']);
-            $this->doPrivMsg($this->config['channel'], $this->hint);
+            $queue->ircPrivmsg($this->config['channel'], $this->hint);
             $this->firstHintTime = null;
             $this->points = ceil($this->points / 2);
         } else if ($this->mode == self::MODE_WAITING && !is_null($this->secondHintTime) && $time >= $this->secondHintTime) {
-            $this->doPrivMsg($this->config['channel'], $this->getHint($this->question['answer'], $this->hint));
+            $queue->ircPrivmsg($this->config['channel'], $this->getHint($this->question['answer'], $this->hint));
             $this->secondHintTime = null;
             $this->points = ceil($this->points / 2);
         } else if ($this->mode == self::MODE_WAITING && !is_null($this->doneTime) && $time >= $this->doneTime) {
-            $this->doPrivMsg($this->config['channel'], 'Times up!');
+            $queue->ircPrivmsg($this->config['channel'], 'Times up!');
             $this->doneTime = null;
-            $this->missed();
+            $this->missed($event, $queue);
         } else if ($this->mode == self::MODE_WAITING && !is_null($this->nextTime) && $time >= $this->nextTime) {
             $this->nextTime = null;
-            $this->next();
+            $this->next($event, $queue);
         }
     }
 
-    private function correct(UserEventInterface $event) {
+    private function correct(UserEventInterface $event, EventQueueInterface $queue)
+    {
         $nick = $event->getNick();
-        $this->doPrivMsg($this->config['channel'], 'Correct! ' . $nick . ' gets ' . $this->points . ' points');
+        $queue->ircPrivmsg($this->config['channel'], 'Correct! ' . $nick . ' gets ' . $this->points . ' points');
 
         $user = User::where('nick', $nick)->first();
         if ($user === null) {
@@ -113,47 +114,44 @@ class Plugin extends AbstractPlugin {
         $user->save();
         $this->next();
     }
-    
-    private function missed() {
-        $this->doPrivMsg($this->config['channel'], 'Were you sleeping? Sit here and study the correct answer.');
-        $this->doPrivMsg($this->config['channel'], 'Answer: ' . $this->question['answer']);
+
+    private function missed(UserEventInterface $event, EventQueueInterface $queue)
+    {
+        $queue->ircPrivmsg($this->config['channel'], 'Were you sleeping? Sit here and study the correct answer.');
+        $queue->ircPrivmsg($this->config['channel'], 'Answer: ' . $this->question['answer']);
         $this->nextTime = time() + 5;
     }
 
-    private function next() {
+    private function next(UserEventInterface $event, EventQueueInterface $queue)
+    {
         $this->mode = self::MODE_ON;
         $this->firstHintTime = null;
-	$this->secondHintTime = null;
+        $this->secondHintTime = null;
         $this->doneTime = null;
         $this->nextTime = null;
         $this->hint = '';
-        $this->ask();  
+        $this->ask($event, $queue);
     }
 
-    private function stop() {
+    private function stop(UserEventInterface $event, EventQueueInterface $queue)
+    {
         $this->mode = self::MODE_OFF;
-        $this->doPrivMsg($this->config['channel'], 'Stopping Trivia');
+        $queue->ircPrivmsg($this->config['channel'], 'Stopping Trivia');
     }
 
-    private function ask() {
+    private function ask(UserEventInterface $event, EventQueueInterface $queue)
+    {
         $this->mode = self::MODE_ASKING;
         $this->question = Question::inRandomOrder()->first();
-        $this->doPrivMsg($this->config['channel'], 'Here comes another question');
-        $this->doPrivMsg($this->config['channel'], $this->question['question']);
-        
+        $queue->ircPrivmsg($this->config['channel'], 'Here comes another question');
+        $queue->ircPrivmsg($this->config['channel'], $this->question['question']);
+
         $this->mode = self::MODE_WAITING;
 
         $this->firstHintTime = time() + $this->config['time_limit_first_hint'];
         $this->secondHintTime = time() + $this->config['time_limit_first_hint'] + $this->config['time_limit_second_hint'];
         $this->doneTime = time() + $this->config['time_limit_first_hint'] + $this->config['time_limit_second_hint'] + $this->config['time_limit_done'];
 
-        $this->doPrivMsg($this->config['channel']);
         $this->points = 3;
     }
-
-    private function doPrivMsg(EventQueueInterface $queue, $target, $message)
-    {
-        $queue->ircPrivmsg($target, $message);
-    }
 }
-
